d851ec45d1da521e4069342e2c05e36e3a72e536
Maxim Kudinov <max.kudinov@proton.me>
2024-08-11 13:58:12 +0300

    Remap navigation and fix typo

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
         systemverilog = { 'verilator' },
-        verilog = { 'veriloator ' },
+        verilog = { 'verilator' },
       }
 
       local verilator = require('lint').linters.verilator
